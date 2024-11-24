5a5a41b5e044a14c34fb2a6f1b466c516c8337c6
bridouze <justin.bouvet@insa-rennes.fr>
2024-11-24 20:26:26 +0100

    "initial commit"

---
+++ script.js
@@ -0,0 +1,13 @@
+// Simple Scroll Effect
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const targetId = e.target.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+    
